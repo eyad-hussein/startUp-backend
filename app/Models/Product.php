ed971748b4170ecb2e57644efec152166eed6fcb
eyad-hussein <eyad.hussein@ejust.edu.eg>
2024-01-31 17:24:45 +0200

    stuck on server error

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -22,4 +22,9 @@ class Product extends Model
     {
         return $this->belongsTo(Image::class);
     }
+
+    public function subimages()
+    {
+        return $this->hasMany(Subimage::class);
+    }
 }
