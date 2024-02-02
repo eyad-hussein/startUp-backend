b5a06beae7152b816d2106de9119058098610e03
eyad-hussein <eyad.hussein@ejust.edu.eg>
2024-02-02 21:16:04 +0200

    Finish initial controller, requests, etc and made sure relationship is correct

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -2,17 +2,40 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\Image\StoreImageRequest;
+use App\Http\Requests\Image\UpdateImageRequest;
+use App\Http\Resources\Image\ImageResource;
+use App\Http\Resources\Image\ImageCollection;
 use App\Models\Image;
+
 
 class ImageController extends Controller
 {
-    public function index()
+    public function index(): ImageCollection
     {
-        $images = Image::all();
-
-        return response([
-            "images" => $images,
-        ]);
+        return new ImageCollection(Image::all());
     }
+
+    public function store(StoreImageRequest $request): ImageResource
+    {
+        $image = Image::create($request->validated());
+        return response()->json($image, 201);
+    }
+
+    public function show(Image $image): ImageResource
+    {
+        return new ImageResource($image);
+    }
+    public function update(UpdateImageRequest $request, Image $image): ImageResource
+    {
+        $image->update($request->validated());
+        return new ImageResource($image);
+    }
+
+    public function destroy(Image $image)
+    {
+        $image->delete();
+        return response()->json(['message' => 'Image deleted successfully']);
+    }
+
 }
