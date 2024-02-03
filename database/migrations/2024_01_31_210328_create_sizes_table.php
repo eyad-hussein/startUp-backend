71680d21c04a9420e985b10b6fdae8c39711e303
eyad-hussein <eyad.hussein@ejust.edu.eg>
2024-02-03 15:20:52 +0200

    Finish initial controller, requests, etc and made sure relationship is correct

--- database/migrations/2024_01_31_210328_create_sizes_table.php
+++ database/migrations/2024_01_31_210328_create_sizes_table.php
@@ -12,8 +12,6 @@ return new class extends Migration {
     {
         Schema::create('sizes', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('product_id');
-            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
             $table->string('size');
             $table->timestamps();
         });
