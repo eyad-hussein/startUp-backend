e751605411abe54a6d79d77a5dab3b32c1763993
eyad-hussein <eyad.hussein@ejust.edu.eg>
2024-02-05 20:32:03 +0200

    Refresh commit

--- app/Models/User.php
+++ app/Models/User.php
@@ -31,6 +31,8 @@ class User extends Authenticatable implements MustVerifyEmail
     protected $hidden = [
         'password',
         'remember_token',
+        'updated_at',
+        'created_at',
     ];
 
     /**
