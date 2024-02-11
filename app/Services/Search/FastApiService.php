c528aca447c74e5c339bfe7354cc6490e2b64494
eyad-hussein <eyad.hussein@ejust.edu.eg>
2024-02-11 04:57:49 +0200

    Create fastapiservice and refactored imagesearchservice and imagesearchcontroller

--- app/Services/Search/FastApiService.php
+++ app/Services/Search/FastApiService.php
@@ -4,5 +4,17 @@ namespace App\Services\Search;
 
 class FastApiService
 {
+    // what is the type of $image?
+    public function retrieveSimilarImagesWithAccuracties(object $image)
+    {
+
+        // logic for retrieving similar images with accuracies from fastapi
+        $result = [
+            "images" => [
+                "image1" => ["url" => "link", "accuracy" => 0.9],
+            ],
+        ];
+        return $result;
+    }
 
 }
